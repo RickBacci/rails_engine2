60d7f44cdba38525fac758e1e4e2568bc070bc02
Rick Bacci <rickbacci@gmail.com>
2015-09-14 21:54:51 -0600

    add invoices controller

---
+++ app/controllers/api/v1/invoices_controller.rb
@@ -0,0 +1,31 @@
+class Api::V1::InvoicesController < ApplicationController
+  respond_to :json
+
+  def index
+    respond_with Invoice.all
+  end
+
+  def show
+    respond_with Invoice.find(params[:id])
+  end
+
+  def find
+    respond_with Invoice.find_by(find_params)
+  end
+
+  def find_all
+    respond_with Invoice.where(find_params)
+  end
+
+  def random
+    respond_with Invoice.all.sample
+  end
+
+  private
+
+  def find_params
+    params.permit(:status,
+                  :created_at,
+                  :updated_at)
+  end
+end
