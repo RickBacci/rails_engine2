f918034bf91a1ade967f2bf5d85028fb558db2e5
Rick Bacci <rickbacci@gmail.com>
2015-09-14 22:07:00 -0600

    add transactions controller

---
+++ app/controllers/api/v1/transactions_controller.rb
@@ -0,0 +1,33 @@
+class Api::V1::TransactionsController < ApplicationController
+  respond_to :json
+
+  def index
+    respond_with Transaction.all
+  end
+
+  def show
+    respond_with Transaction.find(params[:id])
+  end
+
+  def find
+    respond_with Transaction.find_by(find_params)
+  end
+
+  def find_all
+    respond_with Transaction.where(find_params)
+  end
+
+  def random
+    respond_with Transaction.all.sample
+  end
+
+  private
+
+  def find_params
+    params.permit(:id,
+                  :credit_card_number,
+                  :result,
+                  :created_at,
+                  :updated_at)
+  end
+end
