26997fd17dffb2862d168cd7292acf234a893ed0
Rick Bacci <rickbacci@gmail.com>
2015-09-14 21:18:36 -0600

    create record endpoint routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,56 +1,20 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  namespace :api do
+    namespace :v1, defaults: { format: :json } do
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+      categories = ['merchants', 'invoices',
+                    'items', 'invoice_items',
+                    'customers', 'transactions']
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+      categories.each do |category|
+        get "/#{category}/find_all", to: "#{category}#find_all"
+        get "/#{category}/random",   to: "#{category}#random"
+        get "/#{category}/:id",      to: "#{category}#show", constraints: { id: /\d+/ }
+        get "/#{category}/find",     to: "#{category}#find"
+        get "/#{category}",          to: "#{category}#index"
+      end
+    end
+  end
 end
