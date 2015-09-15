91c7497cfa51b06abb99743e551ce825ce22f356
Rick Bacci <rickbacci@gmail.com>
2015-09-14 21:26:11 -0600

    create InvoiceItems controller

---
+++ app/controllers/api/v1/invoice_items_controller.rb
@@ -0,0 +1,32 @@
+class Api::V1::InvoiceItemsController < ApplicationController
+  respond_to :json
+
+  def index
+    respond_with InvoiceItem.all
+  end
+
+  def show
+    respond_with InvoiceItem.find(params[:id])
+  end
+
+  def find
+    respond_with InvoiceItem.find_by(find_params)
+  end
+
+  def find_all
+    respond_with InvoiceItem.where(find_params)
+  end
+
+  def random
+    respond_with InvoiceItem.all.sample
+  end
+
+  private
+
+  def find_params
+    params.permit(:quantity,
+                 :unit_price,
+                 :created_at,
+                 :updated_at)
+  end
+end
